2efb471a8fcb232db878289c220eb7036c949d32
Cynthia Glardon <cyglardo@c3r2s1.42lausanne.ch>
2025-09-25 16:19:17 +0200

    nope

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -4,11 +4,24 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
+    int i;
+    int space;
+
     if (argc < 2)
     {
         cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
         return 0;
     }
-    cout << argv[1];
+    i = 1;
+    space = 0;
+    while (argv[i])
+    {
+        if (space)
+            cout << ' ';
+        cout << argv[i];
+        if (argv[i + 1])
+            space = 1;
+        i ++;
+    }
     return 0;
 }
