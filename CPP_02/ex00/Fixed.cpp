efc9250a3ef7f0aac1c7a5bf42ff07b8412a2fce
orifcg <cynthia.glardon@formation.orif.ch>
2025-10-15 13:52:13 +0000

    bases cpp2 ex00

---
+++ CPP_02/ex00/Fixed.cpp
@@ -0,0 +1,19 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+	this->fixedPt = 0;
+}
+
+Fixed::~Fixed()
+{}
+
+int		Fixed::getRawBits( void ) const
+{
+
+}
+
+void	Fixed::setRawBits( int const raw )
+{
+
+}
---
+++ CPP_02/ex00/Fixed.hpp
@@ -0,0 +1,20 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+
+using namespace std;
+
+class	Fixed
+{
+	public:
+		Fixed();
+		~Fixed();
+		int		getRawBits( void ) const;
+		void	setRawBits( int const raw );
+	private:
+		int					fixedPt;
+		static const int	fractionnalBits;
+};
+
+#endif
