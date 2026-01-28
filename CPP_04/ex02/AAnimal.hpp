64085c898718d6b9ca6edbbad96740ef49427880
Cynthia Glardon <cyglardo@c1r13s4.42lausanne.ch>
2026-01-28 11:22:59 +0100

    updated readme and fixed brain

--- CPP_04/ex02/AAnimal.hpp
+++ CPP_04/ex02/AAnimal.hpp
@@ -7,7 +7,7 @@
 # define DEFAULT "\001\033[0;39m\002"
 # define YELLOW "\001\033[1;93m\002"
 
-//une fonction membre est passee en pure virtual ("virtual" + " = 0") pour qu'on ne puisse plus instancier la classe (l'eval exige que ce soit makeSound).
+//une fonction membre est passee en pure virtual ("virtual" + " = 0") pour qu'on ne puisse plus instancier la classe (l'eval exige que makeSound le soit, meme si on a deja getBrain).
 // On peut aussi passer les constructeurs en protected mais pas necessaire.
 
 //le deconstructeur doit etre virtual pour eviter les comportements inattendus venant du melange delete et polymorphisme.
@@ -21,7 +21,7 @@ class   AAnimal
 		std::string		getType(void) const;
 		void	        setType(std::string type);
     
-		virtual Brain   &getBrain(void) const;
+		virtual Brain   &getBrain(void) const = 0;
     
         virtual void    makeSound() const = 0;
 
